d9977e4b5d47007ed08826ea4e9536f709cd9f5c
vjdavid <vjdavid621@gmail.com>
2015-04-24 13:39:08 -0700

    created seed agent project and task for testing relationships between all models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Agent.create(name: "holis", email: "example@holis.com")
+Agent.create(name: "sample", email: "hols@example.com")
+Agent.create(name: "crayolis", email: "fail@email.com")
+
+Project.create(name: "project", description: "this is my description")
+Project.create(name: "hollis", description: "fake description")
+Project.create(name: "sample", description: "atiiii")
+
+Task.create(name: "project", description: "this is my description")
+Task.create(name: "hollis", description: "fake description")
+Task.create(name: "sample", description: "atiiii")
+
