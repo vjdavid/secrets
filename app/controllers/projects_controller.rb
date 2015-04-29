d384d56575e4a1ccb073a74d82e28718691c503b
vjdavid <vjdavid@gmail.com>
2015-04-29 11:28:03 -0700

    completed projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,49 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :update, :destroy]
+
+  def index
+    if params[:agent_id]
+      @projects = Project.where(agent_id: params[:agent_id])
+      render json: @projects
+    else
+      @projects = Project.all
+      render json: @projects
+    end
+  end
+
+  def show
+    render json: @project
+  end
+
+  def create
+    @project = Project.create(project_params)
+
+    if @project.save
+      render json: @project
+    else
+      render json: @project.errors
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+      render json: @project
+    else
+      render json: @project.errors
+    end
+  end
+
+  def destroy
+    @project.destroy
+    head :no_content
+  end
+
+  private
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
