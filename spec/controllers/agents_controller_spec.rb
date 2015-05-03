dda6dbf98cfc12eca059bb7f19062b02fc878994
vjdavid <vjdavid621@gmail.com>
2015-05-02 19:12:56 -0700

    added spec actions of index and show

--- spec/controllers/agents_controller_spec.rb
+++ spec/controllers/agents_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
+require 'pry'
 
-RSpec.describe Agent, :type => :controller do
+RSpec.describe AgentsController, :type => :controller do
+
+  describe "GET #index" do
+    it "display all agents" do
+      5.times { FactoryGirl.create(:agent) }
+
+      get :index, { :id => 1 }
+      body = JSON.parse(response.body)
+      expect(body.count).to eq(5)
+    end
+  end
+
+  describe "GET #show" do
+    it "display one agent" do
+      agent = FactoryGirl.create(:agent)
+
+      get :show, { :id => agent.id }
+      body = JSON.parse(response.body)
+      expect(agent.id).to eq(agent.id)
+    end
+  end
 
 end
