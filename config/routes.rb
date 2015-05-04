6d4e310e568336cdc627417993a3b9f15676b963
vjdavid <vjdavid@gmail.com>
2015-05-04 14:22:53 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
    member do
      get 'current_project'
    end
+
+   resources :projects
  end
 
  resources :tasks, except: [:new, :edit]
- match 'tasks/:project_id' => 'tasks#index', :via => :get
 
- resources :projects, except: [:new, :edit]
- match 'projects/:agent_id' => 'projects#index', :via => :get
+ resources :projects, except: [:new, :edit] do
+   resources :tasks
+ end
 
 end
