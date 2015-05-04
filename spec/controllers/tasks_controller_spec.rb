6e682eded1febbfcebcc394cef9bb22e13be2e12
vjdavid <vjdavid621@gmail.com>
2015-05-03 22:30:25 -0700

    finished spec tasks controller

---
+++ spec/controllers/tasks_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+require 'pry'
+
+RSpec.describe TasksController, :type => :controller do
+
+  describe "GET #index" do
+    it "return all tasks" do
+     project = FactoryGirl.create(:project)
+     3.times { FactoryGirl.create(:task, project_id: project.id) }
+
+     get :index, { project_id: project.id }
+     body = JSON.parse(response.body)
+     expect(body.count).to eq(3)
+    end
+  end
+
+  describe "GET #show" do
+    it "show one task" do
+      task = FactoryGirl.create(:task)
+
+      get :show, {id: task.id }
+      body = JSON.parse(response.body)
+      expect(body["id"]).to eq(task.id)
+    end
+  end
+
+  describe "POST #create" do
+    it "create one task" do
+      expect {
+        post :create, agent: FactoryGirl.attributes_for(:agent)
+        body = JSON.parse(response.body)
+      }.to change(Task, :count).by(1)
+    end
+  end
+
+  describe "PUT #update" do
+    it "update on task" do
+      task = FactoryGirl.create(:task)
+      old_name = task.name
+
+      put :update, { id: task.id, name: "sample" }
+      body = JSON.parse(response.body)
+      new_name = body["name"]
+      expect(new_name).not_to eq(old_name)
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "delete one task" do
+      task = FactoryGirl.create(:task)
+
+      delete :destroy, { id: task.id }
+      expect(response.body).to eq("")
+    end
+  end
+
+end
