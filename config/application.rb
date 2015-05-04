60c668907af577cd87f0a6df505aaf1ec991decc
vjdavid <vjdavid621@gmail.com>
2015-05-03 22:32:28 -0700

    changed configuration of request_specs on file application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Secret
       helper_specs: false,
       routing_specs: false,
       controller_specs: true,
-      request_specs: false
+      request_specs: true
     g.fixture_replacement :factory_girl, dir: "spec/factories"
   end
 
