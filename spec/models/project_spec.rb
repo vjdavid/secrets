fdb7a6b7743582051966d0046de135b98306c480
vjdavid <vjdavid@gmail.com>
2015-05-06 10:28:57 -0700

    added validations on spec project, name and description

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Project, :type => :model do
   it { should belong_to(:agent) }
   it { should have_many(:tasks) }
   it { should have_one(:assigned_agent) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
 
 end
