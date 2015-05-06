861f86de22af0fe5896eb20773295f11a9eab75f
vjdavid <vjdavid@gmail.com>
2015-05-06 10:28:09 -0700

    added validations on spec agent name and email

--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Agent, :type => :model do
 
   it { should have_many(:projects) }
   it { should belong_to(:current_project) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
 
 end
