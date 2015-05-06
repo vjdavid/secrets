63667f57f3b5c1f4d7829bf8266f4abf0791dfe3
vjdavid <vjdavid@gmail.com>
2015-05-06 10:29:38 -0700

    added validations on spec task, name and description

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Task, :type => :model do
+
   it { belong_to(:project) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+
 end
