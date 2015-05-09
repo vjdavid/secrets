49e91546f5b14670de27dbab1ddf8a86f1fe5f28
vjdavid <vjdavid@gmail.com>
2015-05-08 17:08:49 -0700

    added generate token on model agent and validates password

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,6 +1,19 @@
 class Agent < ActiveRecord::Base
+  before_validation :generate_token,   on: :create
+
   has_many :projects
-  belongs_to :current_project, class: Project
-  validates :name, presence: true
-  validates :email, presence: true
+  belongs_to :current_project,      class: Project
+
+  validates :name,                  presence: true
+  validates :email,                 presence: true
+  validates :password,              presence: true
+
+  private
+
+  def generate_token
+    begin
+      self.token = SecureRandom.hex
+    end while self.class.exists?(token: token)
+  end
+
 end
