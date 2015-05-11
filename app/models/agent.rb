dedf45b0922688fa9331b8073a8f44d65a77bed2
vjdavid <vjdavid621@gmail.com>
2015-05-11 10:03:15 -0700

    added secure password, and validates name email password and token

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,12 +1,15 @@
 class Agent < ActiveRecord::Base
-  before_validation :generate_token,   on: :create
+  has_secure_password validations: false
+
+  before_validation :generate_token,                    on: :create
 
   has_many :projects
-  belongs_to :current_project,      class: Project
+  belongs_to :current_project,                          class: Project
 
-  validates :name,                  presence: true
-  validates :email,                 presence: true
-  validates :password,              presence: true
+  validates :name,                                      presence: true
+  validates :email,                   presence: true, uniqueness: true
+  validates :password,                                  presence: true
+  validates :token,                                     presence: true
 
   private
 
