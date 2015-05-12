e0efd3adf767a552797e123f1fd85487ea7f1090
vjdavid <vjdavid621@gmail.com>
2015-05-12 00:47:08 -0700

    added deny access method and include HttpAuthentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  private
+  def deny_access
+    authenticate_or_request_with_http_token do |token, options|
+      Agent.find_by(token: token)
+    end
+  end
+
 end
