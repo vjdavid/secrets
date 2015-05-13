38d41ad2eb59b03a18c99afe42aeb30dd62741e9
vjdavid <vjdavid@gmail.com>
2015-05-13 12:29:53 -0700

    added rescue standardError for catch all exceptions and filter by regexp http_authorization if correct show current_agent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,24 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  rescue_from StandardError do |exception|
+    render json: { message: "Internal Server Error"}
+  end
 
   def authenticate
-
-    token_authorization = /Token token="(\w{32}+)"/.match(request.env["HTTP_AUTHORIZATION"])
-    token_string = token_authorization[1]
-
-    unless token_string
-      render json: "No HTTP_AUTHORIZATION or header not found"
+    unless request.env.key? "HTTP_AUTHORIZATION"
+      return render json: { message: "HTTP_AUTHORIZATION header not found" }
     end
 
-    unless @current_agent = Agent.find_by(token: token_string)
-      render json: "No agent matching with this token"
+    unless token_authorization = /Token token="(\w{32}+)"/.match(request.env["HTTP_AUTHORIZATION"])
+      return render json: { message: "Incompatible HTTP_AUTHORIZATION header" }
+    end
+
+    unless @current_agent = Agent.find_by(token: token_authorization[1])
+      return render json: { message: "No agent matching with this token" }
     end
 
     @current_agent
   end
 
-end
+ end
