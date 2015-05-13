1f85bc2dec3c943a1660299d417f82d3156fc115
vjdavid <vjdavid@gmail.com>
2015-05-13 12:31:20 -0700

    added validation only when create presence true

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,17 +1,15 @@
 class Agent < ActiveRecord::Base
   has_secure_password validations: false
 
-  before_validation :generate_token,                    on: :create
+  before_validation :generate_token,                       on: :create
 
   has_many :projects
   belongs_to :current_project,                          class: Project
 
   validates :name,                                      presence: true
   validates :email,                   presence: true, uniqueness: true
-  validates :password,                                  presence: true
+  validates :password,                     presence: true, on: :create
   validates :token,                                     presence: true
-
-  private
 
   def generate_token
     begin
