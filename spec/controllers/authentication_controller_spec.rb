a595ea8c912570a6c89d2ead34ba5e00f027ba25
vjdavid <vjdavid@gmail.com>
2015-05-15 17:51:59 -0700

    added spec action of register

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe AuthenticationController, :type => :controller do
 
+  describe "POST #register" do
+    it "join one agent" do
+      agent = FactoryGirl.create(:agent, password: "holis")
+      expect {
+        post :register, FactoryGirl.attributes_for(:agent, password: "holis")
+        body = JSON.parse(response.body)
+      }.to change(Agent, :count).by(1)
+    end
+  end
+
+  describe "POST #login" do
+    it "login one agent" do
+      agent = FactoryGirl.create(:agent, password: "trolis")
+      expect {
+        post :login, FactoryGirl.create(:agent, password: "trolis")
+        body = JSON.parse(response.body)
+      }
+    end
+  end
+
+  describe "POST #logout" do
+    it "signout one agent" do
+    end
+  end
+
 end
