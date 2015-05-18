6ec824051076dcf738ce2928f6b90a9a9af7dc95
vjdavid <vjdavid621@gmail.com>
2015-05-17 18:22:01 -0700

    finished action login and added action logout

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -14,16 +14,23 @@ RSpec.describe AuthenticationController, :type => :controller do
 
   describe "POST #login" do
     it "login one agent" do
-      agent = FactoryGirl.create(:agent, password: "trolis")
-      expect {
-        post :login, FactoryGirl.create(:agent, password: "trolis")
-        body = JSON.parse(response.body)
-      }
+      agent = FactoryGirl.create(:agent, password: "nolis")
+      agent_extra = FactoryGirl.create(:agent, email: "holis@trolis.com", password: "trolis")
+
+      post :login, { email: agent.email, password: agent.password }
+      body = JSON.parse(response.body)
+      expect(agent).not_to eq(agent_extra)
     end
   end
 
   describe "POST #logout" do
     it "signout one agent" do
+      agent = FactoryGirl.create(:agent, password: "holis")
+      old_token = agent.token
+      new_token = SecureRandom.hex
+
+      post :logout, { id: 1, token: new_token }
+      expect(old_token).not_to eq(new_token)
     end
   end
 
