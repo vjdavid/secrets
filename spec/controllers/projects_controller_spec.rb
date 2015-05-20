dc60294e32b87e425f3a822da2f6ea702f9ddb34
vjdavid <vjdavid621@gmail.com>
2015-05-19 22:40:01 -0700

    added http header authentication on all spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,13 +3,17 @@ require 'pry'
 
 RSpec.describe ProjectsController, :type => :controller do
 
+
   describe "GET #index" do
     it "return all projects belongs to agent" do
+
       first_agent = FactoryGirl.create(:agent, password: "holis")
       5.times { FactoryGirl.create(:project, agent_id: first_agent.id) }
 
       additional_agent = FactoryGirl.create(:agent, password: "crayolis")
       5.times { FactoryGirl.create(:project, agent_id: additional_agent.id) }
+
+      request.env['HTTP_AUTHORIZATION'] = "Token token=\"#{ first_agent.token }\""
 
       get :index, { agent_id: first_agent.id }
       body = JSON.parse(response.body)
@@ -29,6 +33,8 @@ RSpec.describe ProjectsController, :type => :controller do
       agent = FactoryGirl.create(:agent)
       5.times { FactoryGirl.create(:project, agent_id: agent.id) }
 
+      request.env['HTTP_AUTHORIZATION'] = "Token token=\"#{ agent.token }\""
+
       get :index
       body = JSON.parse(response.body)
       expect(body.count).to eq(20)
@@ -40,6 +46,8 @@ RSpec.describe ProjectsController, :type => :controller do
       agent = FactoryGirl.create(:agent)
       project = FactoryGirl.create(:project, agent_id: agent.id)
 
+      request.env['HTTP_AUTHORIZATION'] = "Token token=\"#{ agent.token }\""
+
       get :show, { id: project.id }
       body = JSON.parse(response.body)
       expect(body["id"]).to eq(project.id)
@@ -49,6 +57,9 @@ RSpec.describe ProjectsController, :type => :controller do
   describe "POST #create" do
     it "create one project" do
       agent = FactoryGirl.create(:agent)
+
+      request.env['HTTP_AUTHORIZATION'] = "Token token=\"#{ agent.token }\""
+
       expect {
         post :create, FactoryGirl.attributes_for(:project, agent_id: agent.id)
       }.to change(Project, :count).by(1)
@@ -61,6 +72,8 @@ RSpec.describe ProjectsController, :type => :controller do
       project = FactoryGirl.create(:project, agent_id: agent.id)
       old_name = project.name
 
+      request.env['HTTP_AUTHORIZATION'] = "Token token=\"#{ agent.token }\""
+
       put :update, { id: project.id, name: "holis" }
       body = JSON.parse(response.body)
       new_name = body["name"]
@@ -72,6 +85,8 @@ RSpec.describe ProjectsController, :type => :controller do
     it "delete one project" do
       agent = FactoryGirl.create(:agent)
       project = FactoryGirl.create(:project, agent_id: agent.id)
+
+      request.env['HTTP_AUTHORIZATION'] = "Token token=\"#{ agent.token }\""
 
       expect {
         delete :destroy, { id: project.id }
