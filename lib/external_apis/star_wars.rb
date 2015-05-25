a1c95a1e0efda3cc82ed46d2726b06ca9cab0df6
vjdavid <vjdavid@gmail.com>
2015-05-25 14:42:48 -0700

    added class and methos for cosume swapi

--- lib/external_apis/star_wars.rb
+++ lib/external_apis/star_wars.rb
@@ -1,2 +1,14 @@
-module ExternalApis::StarWars
+class ExternalApis::StarWars
+  include HTTParty
+
+  base_uri 'swapi.co/api'
+
+  def show_starships
+    self.class.get("/starships/")
+  end
+
+  def show_planets
+    self.class.get("/planets/")
+  end
+
 end
