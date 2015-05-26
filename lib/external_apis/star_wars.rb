eb7d852f45a16339cfd176f6bd72220568ae4e55
vjdavid <vjdavid@gmail.com>
2015-05-26 14:07:27 -0700

    added marvel configurations

--- lib/external_apis/star_wars.rb
+++ lib/external_apis/star_wars.rb
@@ -1,14 +1,20 @@
 class ExternalApis::StarWars
   include HTTParty
 
-  base_uri 'swapi.co/api'
+  base_uri 'gateway.marvel.com'
 
-  def show_starships
-    self.class.get("/starships/")
+  def authentication
+    Digest::MD5.hexadigest("#{st}#{private_key}#{public_key}")
   end
 
-  def show_planets
-    self.class.get("/planets/")
+  def st
+    begin
+      Time.now.to_i
+    end
+  end
+
+  def show_characters
+    self.class.get('/v1/public/characters/' authentication)
   end
 
 end
